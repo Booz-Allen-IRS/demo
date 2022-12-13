b93d1a8dddba7c00b882145eb14c30a2db20cd52
wgoodwinjr <106978134+wgoodwinjr@users.noreply.github.com>
2022-12-13 12:06:52 -0500

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,8 @@
+# variable "snowflake_key" {}
+
+variable "SNOWFLAKE_ACCOUNT" {}
+variable "SNOWFLAKE_USER" {}
+variable "SNOWFLAKE_PASSWORD" {}
+variable "SNOWFLAKE_REGION" {}
+variable "snowflake_key" {}
+variable "ASTRA_API_TOKEN" {}
